c2d09cd3d01f45a88812d5e45b178dace4ca5775
akankshad12 <akankshadhoka@gmail.com>
2020-11-06 11:35:57 -0800

    1

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Home from './components/pages/Home'
+import NotFound from './components/pages/NotFound'
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import User from "./components/users/User";
+import "./App.css";
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
+
+const App = () => {
+    //from state passing as this.props.
+    return (
+      <GithubState>
+      <AlertState>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert/>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                component = {Home}
+              />
+              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                component = {User}
+              />
+              <Route component = {NotFound}></Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+      </AlertState>
+      </GithubState>
+    );
+  }
+
+export default App;
